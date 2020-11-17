d9efee86b82697840d61ca0b3acf42e156f2b161
Chris Blum <cblum@redhat.com>
2020-11-17 14:27:02 +0100

    Implement ReplicaSet
    Resolves #4

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 class Cluster {
-	servers: Array<Server>;
+	replicaSets: Array<ReplicaSet>;
 	platform: string;
 	diskType: NVMe;
 	static replicaCount = 3;
@@ -7,59 +7,115 @@ class Cluster {
 	constructor(platform: string, diskType: NVMe, targetCapacity: number) {
 		this.platform = platform;
 		this.diskType = diskType;
-		this.servers = [];
-		for (let i = 0; i < 3; i++) {
-			this.addServer()
-		}
-		this.servers[0].addService(new Ceph_MGR)
-		this.servers[0].addService(new Ceph_MON)
-		this.servers[1].addService(new Ceph_MON)
-		this.servers[2].addService(new Ceph_MON)
-		this.servers[1].addService(new Ceph_RGW)
-		this.servers[2].addService(new Ceph_RGW)
-		this.servers[1].addService(new Ceph_MDS)
-		this.servers[2].addService(new Ceph_MDS)
-		this.servers[0].addService(new Noobaa_DB)
-		this.servers[1].addService(new Noobaa_Endpoint)
-		this.servers[2].addService(new Noobaa_core)
+		this.replicaSets = [new ReplicaSet(this.platform, this.diskType)];
+		// this.addReplicaSet();
+		this.addService(new Ceph_MGR)
+		this.addService(new Ceph_MON)
+		this.addService(new Ceph_RGW)
+		this.addService(new Ceph_MDS)
+		this.addService(new Noobaa_DB)
+		this.addService(new Noobaa_Endpoint)
+		this.addService(new Noobaa_core)
 
-		const osdsNeededForTargetCapacity = Math.ceil(targetCapacity/diskType.maxCapacity()) * Cluster.replicaCount
+		const osdsNeededForTargetCapacity = Math.ceil(targetCapacity / diskType.maxCapacity())
 
 		for (let i = 0; i < osdsNeededForTargetCapacity; i++) {
 			this.addService(new Ceph_OSD)
 		}
 	}
 
-	addServer() {
-		switch (this.platform) {
-			case "metal": return this.servers.push(new BareMetal(this.diskType));
-			case "awsAttached": return this.servers.push(new AWSattached(this.diskType));
-			case "awsEBS": return this.servers.push(new AWSEBS(this.diskType));
-			case "vmware": return this.servers.push(new VMware(this.diskType));
-		}
+	addReplicaSet() {
+		const count = this.replicaSets.push(new ReplicaSet(this.platform, this.diskType))
+		console.log(`Added new Set - now we have ${count}`)
 	}
 
 	addService(service: Service) {
-		// Find server with lowest CPU usage
-		let targetServer = this.servers[0]
-		this.servers.forEach(server => {
-			if (server.getUsedCPU() < targetServer.getUsedCPU()) {
-				targetServer = server
-			}
-		})
-		if (!targetServer.addService(service)) {
-			// If the server refuses to add the service
-			// it is probably already full - then add a new server
-			this.addServer()
+		// Use the replicaSet that was added last
+		// We assume that we fill up replicaSets before we add new ones
+		const targetSet = this.replicaSets[this.replicaSets.length - 1]
+		if (!targetSet.addService(service)) {
+			// If the set refuses to add the service
+			// it is probably already full - then add a new set
+			console.log("ADDING SET")
+			this.addReplicaSet()
 			this.addService(service)
 		}
 	}
 	print(indentation = "") {
-		let message = `Each server has ${Object.getPrototypeOf(this.servers[0]).constructor.cpuCores} CPU cores, ${Object.getPrototypeOf(this.servers[0]).constructor.memory} GB memory and a maximum of ${Object.getPrototypeOf(this.servers[0]).constructor.maxDisks} disks.\n`;
+		let message = `Each server has ${Object.getPrototypeOf(this.replicaSets[0].servers[0]).constructor.cpuCores} CPU cores, ${Object.getPrototypeOf(this.replicaSets[0].servers[0]).constructor.memory} GB memory and a maximum of ${Object.getPrototypeOf(this.replicaSets[0].servers[0]).constructor.maxDisks} disks.\n`;
 		message += `The max disk size in this cluster is ${this.diskType.maxCapacity()}\n`;
-		message += `This cluster has ${this.servers.length} servers`;
+		message += `This cluster has ${this.replicaSets.length * Cluster.replicaCount} servers`;
+		for (let i = 0; i < this.replicaSets.length; i++) {
+			message += `\n\nReplicaSet ${i + 1}` + this.replicaSets[i].print(indentation);
+		}
+		return message
+	}
+}
+
+class ReplicaSet {
+	replicaCount: number
+	platform: string
+	diskType: NVMe;
+	servers: Array<Server>
+
+	constructor(platform, diskType) {
+		this.replicaCount = 3
+		this.platform = platform
+		this.diskType = diskType
+		this.servers = []
+		for (let i = 0; i < this.replicaCount; i++) {
+			switch (this.platform) {
+				case "metal": this.servers.push(new BareMetal(this.diskType)); break;
+				case "awsAttached": this.servers.push(new AWSattached(this.diskType)); break;
+				case "awsEBS": this.servers.push(new AWSEBS(this.diskType)); break;
+				case "vmware": this.servers.push(new VMware(this.diskType)); break;
+			}
+		}
+	}
+
+	addService(service: Service) {
+		let returnFlag = true
+		switch (Object.getPrototypeOf(service).constructor) {
+			// Services running on 3 servers
+			case Ceph_MON:
+			case Ceph_OSD:
+				this.servers.forEach(server => {
+					if (!server.canIAddService(service)) {
+						returnFlag = false
+						return
+					}
+				});
+				if (!returnFlag) return false
+				this.servers.forEach(server => {
+					server.addService(service)
+				});
+				return true
+			// Services running on 2 servers
+			case Ceph_MDS:
+			case Ceph_MGR:
+			case Ceph_RGW:
+			case Noobaa_DB:
+			case Noobaa_Endpoint:
+			case Noobaa_core:
+				// Sort servers ascending based on used CPU
+				this.servers.sort(function (a, b) {
+					if (a.getUsedCPU() < b.getUsedCPU()) return -1
+					else return 1
+				});
+				this.servers.slice(0,1).forEach(server => {
+					if (!server.canIAddService(service)) return false
+				});
+				this.servers.slice(0,1).forEach(server => {
+					server.addService(service)
+				});
+				return true
+		}
+	}
+
+	print(indentation = "") {
+		let message = ""
 		for (let i = 0; i < this.servers.length; i++) {
-			message += `\n\nSERVER ${i+1}\n` + this.servers[i].print(indentation);
+			message += `\n${indentation}SERVER ${i + 1}\n` + this.servers[i].print(indentation + indentation);
 		}
 		return message
 	}
