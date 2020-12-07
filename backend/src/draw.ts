02b2bd4a7ad35ee5e11f05fdff5611c5b0fdad62
Chris Blum <cblum@redhat.com>
2020-12-07 13:53:34 +0100

    Fix bug where server and disk image was gone

--- backend/src/draw.ts
+++ backend/src/draw.ts
@@ -40,7 +40,7 @@ export function drawServer(
       left: 40,
       cropY: 140,
       cropX: 30
-    }).scaleToWidth(scale)
+    }).scaleToWidth(200)
   );
 
   group.addWithUpdate(
@@ -49,7 +49,7 @@ export function drawServer(
       left: 40,
       cropY: 100,
       cropX: 30
-    }).scaleToWidth(scale)
+    }).scaleToWidth(100)
   );
 
   group.addWithUpdate(
