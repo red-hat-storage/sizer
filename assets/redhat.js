46e6d0e7dc2d9dfcece3e69e67a71a3eee875390
Chris Blum <cblum@redhat.com>
2021-01-18 15:04:04 +0100

    Only show support exception links for Red Hatters

---
+++ assets/redhat.js
@@ -0,0 +1,9 @@
+const $ = window.jQuery
+
+$(function () {
+    const link = $("#supportExceptionLink")[0]
+    link.innerHTML = `<a style="background-color: white"
+        href="https://tools.apps.cee.redhat.com/support-exceptions/add">
+        open a support exception ticket
+        </a>`
+})
