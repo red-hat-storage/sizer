5124874d508c8ff83aa2edf8e1936ea2385254a8
Chris Blum <cblum@redhat.com>
2021-02-03 13:30:11 +0100

    Set variable directly

--- assets/redhat.js
+++ assets/redhat.js
@@ -1,7 +1,7 @@
 const $ = window.jQuery
+window.redhatter = true
 
 $(function () {
-    window.redhatter = true
     const link = $("#supportExceptionLink")[0]
     link.innerHTML = `<a style="background-color: white"
         href="https://tools.apps.cee.redhat.com/support-exceptions/add">
