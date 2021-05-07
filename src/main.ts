563bf48153825c32aa957726c5c01fe7c7bd48ec
Chris Blum <cblum@redhat.com>
2021-05-07 17:45:57 +0000

    Add i3 to tech-preview platforms
    As discussed in #80

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ function updatePlanning() {
     const message = `Currently we only test disk sizes up to 4TB. While we do not expect any issues with larger disks, we do not currently test this and thus you will need to request a support exception.`;
     addSupportExceptionReason("DiskSize", "Disk exceeds 4TB", message);
   }
-  if (platform == "vmPreview" || platform == "azure" || platform == "gcp") {
+  if (["vmPreview", "azure", "gcp", "awsAttached"].includes(platform)) {
     const message = `You selected a platform that is currently in tech-preview. While we are confident with the results of our testing so far, not all disaster scenarios have been tested and you will need to request a support exception to run on this platform in production.<br>
     We also use these support exceptions to learn about the demand for the various platforms and to priorities which platform will be fully supported next.`;
     addSupportExceptionReason("Platform", "Platform in Tech Preview", message);
