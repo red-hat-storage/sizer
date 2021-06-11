a75368bd20c85d73e98c41a0eb76d418ae2b8fe7
Chris Blum <cblum@redhat.com>
2021-06-11 21:27:25 +0000

    Zones can compute their total CPU usage

--- src/models/Zone.ts
+++ src/models/Zone.ts
@@ -7,4 +7,12 @@ export class Zone {
   constructor(nodes: Node[]) {
     this.nodes = nodes;
   }
+
+  getTotalUsedCPU(): number {
+    let totalCPU = 0;
+    this.nodes.forEach((node) => {
+      totalCPU += node.getUsedCPU();
+    });
+    return totalCPU;
+  }
 }
