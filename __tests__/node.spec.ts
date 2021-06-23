68f436f73c18bf6d2d22c9b2c68fced8ac510274
Chris Blum <cblum@redhat.com>
2021-06-23 12:54:27 +0000

    Run Tests on all types of nodes

--- __tests__/node.spec.ts
+++ __tests__/node.spec.ts
@@ -1,4 +1,11 @@
-import { BareMetal, Node } from "../src/models/Node";
+import {
+  AWSattached,
+  AWSEBS,
+  Azure,
+  BareMetal,
+  GCP,
+  VMnode,
+} from "../src/models/Node";
 import { Service } from "../src/models/Service";
 import { Workload } from "../src/models/Workload";
 
@@ -7,28 +14,35 @@ const services = [
   new Service("B", 3, 3, 3, [], []),
 ];
 
-describe("Test Node common Methods", () => {
-  let node: Node;
+const nodes = [
+  new BareMetal(),
+  new AWSattached(),
+  new AWSEBS(),
+  new Azure(),
+  new GCP(),
+  new VMnode(),
+];
 
+describe.each(nodes)("Test Node common Methods", (node) => {
   beforeEach(() => {
-    node = new BareMetal();
+    node.workloads = {};
   });
 
-  it("Test fiting workload (canIAddWorkload)", () => {
+  it(`Test fiting workload (canIAddWorkload) ${node.constructor.name}`, () => {
     const fittingWorkload = new Workload("smallWorkload", services, 5);
     expect(node.canIAddWorkload(fittingWorkload)).toBeTruthy();
   });
 
-  it("Test larger workload (canIAddWorkload)", () => {
+  it(`Test larger workload (canIAddWorkload) ${node.constructor.name}`, () => {
     const largeWorkload = new Workload(
       "largeWorkload",
-      [new Service("A", 16, 72, 3, [], [])],
+      [new Service("A", 26, 172, 3, [], [])],
       3
     );
     expect(node.canIAddWorkload(largeWorkload)).toBeFalsy();
   });
 
-  it("Test if existing services are disallowed (canIAddWorkload)", () => {
+  it(`Test if existing services are disallowed (canIAddWorkload) ${node.constructor.name}`, () => {
     const workload = new Workload("deployment-A", services, 3);
     const servicesWithDuplicates = [
       services[0],
@@ -44,7 +58,7 @@ describe("Test Node common Methods", () => {
     expect(node.canIAddWorkload(workload)).toBeFalsy();
   });
 
-  it("Test if avoiding of coplacement of services is adhered (canIAddWorkload)", () => {
+  it(`Test if avoiding of coplacement of services is adhered (canIAddWorkload) ${node.constructor.name}`, () => {
     /**
      * Tests whether the avoid from existing services is respected.
      */
@@ -61,14 +75,14 @@ describe("Test Node common Methods", () => {
     expect(node.canIAddWorkload(unsuitableWorkload)).toBeFalsy();
   });
 
-  it("test getAmountOfOSDs", () => {
+  it(`test getAmountOfOSDs ${node.constructor.name}`, () => {
     const cephOSDService = new Service("Ceph_OSD", 6, 6, 3, [], []);
     const simpleWorkload = new Workload("ceph-osd", [cephOSDService], 3);
-    node.addWorkload(simpleWorkload);
+    expect(node.addWorkload(simpleWorkload)).toBeTruthy;
     expect(node.getAmountOfOSDs()).toEqual(1);
   });
 
-  it("Test getDetails", () => {
+  it(`Test getDetails ${node.constructor.name}`, () => {
     const workload = new Workload("deployment-A", services, 3);
     node.addWorkload(workload);
     const {
