20db0ba2f745bd4eff11c785eef870b1e15f761a
Chris Blum <cblum@redhat.com>
2021-11-26 17:07:22 +0100

    Expose editServices

--- src/redux/reducers/service.ts
+++ src/redux/reducers/service.ts
@@ -12,7 +12,7 @@ const defaultState = {
 const addService = createAction<Service>("ADD_SERVICE");
 const addServices = createAction<Service[]>("ADD_SERVICES");
 
-const editService = createAction<Service>("UPDATE_SERVICE");
+const editServices = createAction<Service[]>("UPDATE_SERVICE");
 const removeServices = createAction<Service[]>("REMOVE_SERVICE");
 
 const serviceReducer = createReducer(defaultState, (builder) => {
@@ -23,9 +23,11 @@ const serviceReducer = createReducer(defaultState, (builder) => {
     .addCase(addServices, (state, { payload }) => {
       payload.forEach((item) => state.services.push(item));
     })
-    .addCase(editService, (state, { payload }) => {
-      state.services.map((service) =>
-        service.id === payload.id ? payload : service
+    .addCase(editServices, (state, { payload }) => {
+      payload.forEach((item) =>
+        state.services.map((service) =>
+          service.id === item.id ? item : service
+        )
       );
     })
     .addCase(removeServices, (state, { payload }) => {
@@ -36,4 +38,10 @@ const serviceReducer = createReducer(defaultState, (builder) => {
     });
 });
 
-export { addService, addServices, editService, removeServices, serviceReducer };
+export {
+  addService,
+  addServices,
+  editServices,
+  removeServices,
+  serviceReducer,
+};
