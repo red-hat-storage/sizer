6cc3be73a2b1567ddcac9e0b761c3b677a4d22e7
Chris Blum <cblum@redhat.com>
2021-11-26 17:07:01 +0100

    do not assume code coverage in tsx files

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,7 @@ export default {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
-  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts}"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
