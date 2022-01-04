039096c8affee8fd93574db91fd9f6bab98d3a87
Chris Blum <cblum@redhat.com>
2022-01-04 10:10:02 +0100

    Add maxDisks to instance definition
    Also added comments

--- src/types/common.ts
+++ src/types/common.ts
@@ -24,11 +24,26 @@ export const enum DeploymentType {
 }
 
 export type Instance = {
+  // Human friendly name of this instance type
+  // the way it is known on this platform
+  // e.g. m5.4xlarge
   name: string;
+  // Memory of this instance type in GB
   memory: number;
+  //  CPU Units (threads) of this instance type
   cpuUnits: number;
+  // Zero if no local disks available
+  // Else the TB of one of the local disks
   instanceStorage?: number;
+  // Number of local disks in this instance
+  maxDisks?: number;
+  // True if this is the default worker type
+  // on this platform
   default?: boolean;
+  // True if this is the default control plane
+  // type on this platform
   controlPlane?: boolean;
+  // True if this is the default ODF
+  // type on this platform
   odfDefault?: boolean;
 };
