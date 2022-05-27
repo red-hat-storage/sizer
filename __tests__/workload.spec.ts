e4c5d61a5c83136c817cc4aa1e489fe3f22beb5c
Bipul Adhikari <badhikar@redhat.com>
2022-05-27 13:47:48 +0545

    Add tests for Workload and Service generator Partially fixes Issue #97
    Signed-off-by: Bipul Adhikari <badhikar@redhat.com>

--- __tests__/workload.spec.ts
+++ __tests__/workload.spec.ts
@@ -1,22 +1,53 @@
-import { Service } from "../src/models/Service";
-import { Workload } from "../src/models/Workload";
+import { WorkloadDescriptor } from "../src/types";
+import { getWorkloadFromDescriptors } from "../src/utils";
 
-describe("Test workload Class", () => {
-  let workload: Workload;
-  beforeAll(() => {
-    const services = [
-      new Service("A", 3, 3, 3, [], []),
-      new Service("B", 3, 3, 3, [], []),
-    ];
-    workload = new Workload("testworkload", services, 5);
+const simpleWLDescription: WorkloadDescriptor = {
+  name: "Kafka",
+  count: 1,
+  usesMachines: [],
+  storageCapacityRequired: 1000,
+  services: [
+    {
+      name: "Kafka-Zookeeper",
+      requiredCPU: 4,
+      requiredMemory: 8,
+      zones: 3,
+      runsWith: [],
+      avoid: [],
+    },
+    {
+      name: "Kafka-Broker",
+      requiredCPU: 12,
+      requiredMemory: 64,
+      zones: 3,
+      runsWith: [],
+      avoid: [],
+    },
+    {
+      name: "Kafka-Connect",
+      requiredCPU: 2,
+      requiredMemory: 3,
+      zones: 2,
+      runsWith: [],
+      avoid: [],
+    },
+  ],
+};
+
+describe("Test Workload and Service creation using `getWorkloadFromDescriptors`", () => {
+  const { services, workload } =
+    getWorkloadFromDescriptors(simpleWLDescription);
+  it("Generates services with proper IDs", () => {
+    services.forEach((service) => {
+      expect(service.id).toBeGreaterThanOrEqual(0);
+    });
   });
-  it("getTotalMemory works as expected", () => {
-    expect(workload.getTotalMemory()).toEqual(6);
+
+  it("Generates workload with proper ID", () => {
+    expect(workload.id).toBeGreaterThanOrEqual(0);
   });
-  it("getTotalCPU works as expected", () => {
-    expect(workload.getTotalCPU()).toEqual(6);
-  });
-  it("getNamesOfServices works as expected", () => {
-    expect(workload.getNamesOfServices()).toEqual(["A", "B"]);
+
+  it("Generates workload with correctly assigned Service IDs", () => {
+    expect(workload.services).toEqual(services.map((service) => service.id));
   });
 });
