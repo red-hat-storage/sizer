004c4b4da3429d3edd8256c2cef7ecf0c2915c06
Bipul Adhikari <badhikar@redhat.com>
2022-05-31 19:37:46 +0545

    Add validation for Disk Size Minimum DiskSize should be 0.1 TiB Fixes Issue #121

--- src/components/Storage/DiskSize.tsx
+++ src/components/Storage/DiskSize.tsx
@@ -17,44 +17,50 @@ const DiskSize: React.FC<DiskSizeProps> = ({ machine }) => {
 
   const dispatch = useDispatch();
 
-  const increment =
-    (action: typeof setFlashSize | typeof setUsableCapacity) => () => {
-      let payload = 0;
-      if (action === setFlashSize) {
-        payload = ocsState.flashSize + 0.1;
-      } else {
-        payload = ocsState.usableCapacity + 1;
-      }
-      payload = +payload.toFixed(1);
-      dispatch(action(payload));
-    };
+  const increment = (
+    action: typeof setFlashSize | typeof setUsableCapacity
+  ) => () => {
+    let payload = 0;
+    if (action === setFlashSize) {
+      payload = ocsState.flashSize + 0.1;
+    } else {
+      payload = ocsState.usableCapacity + 1;
+    }
+    payload = +payload.toFixed(1);
+    dispatch(action(payload));
+  };
 
-  const decrement =
-    (action: typeof setFlashSize | typeof setUsableCapacity) => () => {
-      let payload = 0;
-      if (action === setFlashSize) {
-        payload = ocsState.flashSize - 0.1;
-      } else {
-        payload = ocsState.usableCapacity - 1;
-      }
-      payload = +payload.toFixed(1);
-      dispatch(action(payload));
-    };
+  const decrement = (
+    action: typeof setFlashSize | typeof setUsableCapacity
+  ) => () => {
+    let payload = 0;
+    if (action === setFlashSize) {
+      payload = ocsState.flashSize - 0.1;
+    } else {
+      payload = ocsState.usableCapacity - 1;
+    }
+    payload = +payload.toFixed(1);
+    dispatch(action(payload));
+  };
 
-  const changeText =
-    (type: typeof setFlashSize | typeof setUsableCapacity) =>
-    (e: React.FormEvent<HTMLInputElement>) => {
-      const inputValue = Number(e.currentTarget.value);
-      if (type === setFlashSize && inputValue > -1 && inputValue <= 16) {
-        dispatch(setFlashSize(inputValue));
-      }
-      if (type === setUsableCapacity && inputValue > -1 && inputValue <= 1000) {
-        dispatch(setUsableCapacity(inputValue));
-      }
-    };
+  const changeText = (type: typeof setFlashSize | typeof setUsableCapacity) => (
+    e: React.FormEvent<HTMLInputElement>
+  ) => {
+    const inputValue = Number(e.currentTarget.value);
+    if (type === setFlashSize && inputValue > -1 && inputValue <= 16) {
+      dispatch(setFlashSize(inputValue));
+    }
+    if (type === setUsableCapacity && inputValue > -1 && inputValue <= 1000) {
+      dispatch(setUsableCapacity(inputValue));
+    }
+  };
 
-  const isDiskSizeTechPreview = React.useMemo(
-    () => (ocsState.flashSize > 4.0 ? "error" : "default"),
+  const isDiskSizeTechPreview = React.useMemo(() => ocsState.flashSize > 4.0, [
+    ocsState.flashSize,
+  ]);
+
+  const isDiskSizeZero = React.useMemo(
+    () => ocsState.flashSize === 0 || ocsState.flashSize < 0.1,
     [ocsState.flashSize]
   );
 
@@ -62,18 +68,24 @@ const DiskSize: React.FC<DiskSizeProps> = ({ machine }) => {
     ? machine?.instanceStorage === 0 || _.isUndefined(machine?.instanceStorage)
     : true;
 
+  const helperText = isDiskSizeZero
+    ? "Minimum disk size should be of 0.1 TB"
+    : "Disks greater than 4TB is not tested and is still a tech preview feature.";
+
   return (
     <>
       <FormGroup
         label="Flash Disk Size (TB)"
         fieldId="flash-input"
-        validated={isDiskSizeTechPreview}
-        helperTextInvalid="Disks greater than 4TB is not tested and is still a tech preview feature."
+        validated={
+          isDiskSizeTechPreview || isDiskSizeZero ? "error" : "success"
+        }
+        helperTextInvalid={helperText}
         helperTextInvalidIcon={<WarningTriangleIcon />}
       >
         <NumberInput
           value={ocsState.flashSize}
-          min={0}
+          min={0.1}
           max={16}
           onMinus={decrement(setFlashSize)}
           onPlus={increment(setFlashSize)}
