5f5941b6e66a64984c64604e4a94b7697a0c5551
Chris Blum <cblum@redhat.com>
2022-07-28 11:27:36 +0200

    Update Tour text
    Resolves #137

--- src/components/Tour/Tour.ts
+++ src/components/Tour/Tour.ts
@@ -36,8 +36,8 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
 
   tour.addSteps([
     {
-      title: "Welcome to the ODF Sizer",
-      text: "This tool supports ODF 4.5 onwards",
+      title: "Welcome to the Sizer",
+      text: "Configure your Workloads </br>and see your required cluster and storage size",
       buttons: [
         {
           text: "Skip Tour",
@@ -50,8 +50,8 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      title: "The layout",
-      text: "ODF sizer has four tabs. These four tabs are Workloads, Storage, Compute and Results",
+      title: "The main navigation",
+      text: "Use the tabs to navigate between defining your <b>Workloads</b>, configuring required <b>Storage</b>, setting your desired <b>Compute</b> sizes and to view your current Sizer <b>Results</b>",
       attachTo: {
         element: ".pf-c-tabs",
         on: "bottom",
@@ -69,7 +69,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
     },
     {
       title: "Workloads",
-      text: "This page allows you to create workloads to simulate on this OCP cluster.",
+      text: "On this page you add your list of Applications that will run on your cluster. We have some common use cases ready as Templates. Defining custom Workloads is also possible.",
       attachTo: {
         element: generateTabId("workloads-tab", 0),
         on: "bottom",
@@ -87,7 +87,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
     },
     {
       title: "Storage",
-      text: "This page allows you to create an ODF storage cluster.",
+      text: "Here you define the OpenShift Data Foundation installation which will add persistent storage to your Workloads.",
       attachTo: {
         element: generateTabId("storage-tab", 1),
         on: "top",
@@ -110,24 +110,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "Now select how large each disk is. We assume all disks will be of equal size.",
-      attachTo: {
-        element: "#flash-input",
-        on: "top",
-      },
-      buttons: [
-        {
-          text: "Skip Tour",
-          action: tour.cancel,
-        },
-        {
-          text: "Next",
-          action: tour.next,
-        },
-      ],
-    },
-    {
-      text: "At last, chose how much usable capacity you need. This will be the total usable cluster capacity in the ODF cluster",
+      text: "The most important input on this page is the usable storage capacity.",
       attachTo: {
         element: "#usable-input",
         on: "top",
@@ -144,18 +127,10 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      beforeShowPromise: async () => {
-        dispatch(setTab(3));
-        // Set State to make the cluster out of regular support size
-        dispatch(setFlashSize(6));
-        while (!doesElementExist("#support-exception-modal")) {
-          await sleep(100);
-        }
-      },
-      text: "When your configuration is out of the regular support limits, we will show a modal here",
+      text: "Some Workloads estimate their required capacity to help you size the storage cluster properly.",
       attachTo: {
-        element: "#support-exception-modal",
-        on: "bottom",
+        element: ".pf-c-chart",
+        on: "top",
       },
       buttons: [
         {
@@ -167,13 +142,6 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
           action: tour.next,
         },
       ],
-      when: {
-        hide: () => {
-          // Revert it to original State
-          dispatch(setFlashSize(2.5));
-          dispatch(setTab(1));
-        },
-      },
     },
     {
       beforeShowPromise: async () => {
@@ -183,7 +151,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
         }
       },
       title: "Compute",
-      text: "This page allows you to create MachineSets to simulate Nodes on this OCP cluster.",
+      text: "On this page you can set your instance sizes that make up your OpenShift cluster. We have instance sizes for the most popular public clouds built in, otherwise you can use the BareMetal configuration.",
       attachTo: {
         element: generateTabId("compute-tab", 2),
         on: "bottom",
@@ -200,7 +168,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "Chose your Platform here. Note that some Platforms are in tech-preview",
+      text: "Chose your Platform here. If your target Platform is not in the list, try using the BareMetal option.",
       attachTo: {
         element: "#platform-selector",
         on: "top",
@@ -228,7 +196,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
         element: generateTabId("results-tab", 3),
         on: "bottom",
       },
-      text: "Generic information on how we calculate the cluster will be visible here and auto-update when you change the state",
+      text: "Generic information on how we calculate the cluster will be visible here and auto-update when you change inputs on the other tabs.",
       buttons: [
         {
           text: "Skip Tour",
@@ -243,6 +211,38 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
     {
       beforeShowPromise: async () => {
         dispatch(setTab(3));
+        // Set State to make the cluster out of regular support size
+        dispatch(setFlashSize(6));
+        while (!doesElementExist("#support-exception-modal")) {
+          await sleep(100);
+        }
+      },
+      text: "If your configuration is out of the regular support limits, we will warn you on the Results page",
+      attachTo: {
+        element: "#support-exception-modal",
+        on: "bottom",
+      },
+      buttons: [
+        {
+          text: "Skip Tour",
+          action: tour.cancel,
+        },
+        {
+          text: "Next",
+          action: tour.next,
+        },
+      ],
+      when: {
+        hide: () => {
+          // Revert it to original State
+          dispatch(setFlashSize(2.5));
+          dispatch(setTab(1));
+        },
+      },
+    },
+    {
+      beforeShowPromise: async () => {
+        dispatch(setTab(3));
         while (!doesElementExist("#sharing-link")) {
           await sleep(100);
         }
@@ -252,7 +252,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
         element: "#sharing-link",
         on: "top",
       },
-      text: "You can click on this button to generate the sharing link for your configuration.",
+      text: "Clicking on this button will generate a link that you can use to share this exact configuration with your peers.",
       buttons: [
         {
           text: "Skip Tour",
@@ -265,7 +265,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "A more advanced output that includes a view into how we distributed the ODF base service Pods can be toggled with this button.",
+      text: "This button will open up the advanced view, usually reserved for debugging placement issues of Workloads on Machines.",
       attachTo: {
         element: "#advanced-results-button",
         on: "top",
@@ -282,7 +282,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "Finally down here you see your cluster as a picture that you can share. Each column represents a distinct availability zone.",
+      text: 'Down here you see a visual representation of your planned OpenShift cluster. You can download this as a picture by clicking on the "Download Cluster Image" button.',
       attachTo: {
         element: "#nodes-vis-container",
         on: "top",
@@ -299,7 +299,24 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "Each node has the same size, which is displayed here. For public cloud deployments we display our instance type recommendation.",
+      text: "Nodes in the same column are in the same availability zone. We count them up within their zone.</br>You will also see the instance size in public clouds here.",
+      attachTo: {
+        element: ".card-container__title",
+        on: "top",
+      },
+      buttons: [
+        {
+          text: "Skip Tour",
+          action: tour.cancel,
+        },
+        {
+          text: "Next",
+          action: tour.next,
+        },
+      ],
+    },
+    {
+      text: "Below the title we display the name of the Machine Set that deployed this node for your Workloads.",
       attachTo: {
         element: "#instance-type",
         on: "top",
@@ -316,7 +333,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "Here we show how many disks will be used on this node. The amount of disks may vary between rows of nodes.",
+      text: "This counts the number of local disks dedicated to the Storage Cluster.",
       attachTo: {
         element: ".card-container-disk-section__count",
         on: "top",
@@ -333,7 +350,7 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "These bars show you how much CPU and Memory is used up by this deployment. It will estimate what ODF will use up and for compact mode also estimate what OCP will use up at least. <br>If you hover over the bars, it will show more details. <br><br>Be aware that these numbers are just estimations that also rely on the placement of the Pods in your deployment.",
+      text: "These bars show you how much CPU and Memory is used up by the Workloads and Cluster Services.<br><br>Be aware that these numbers are just estimations that also rely on the placement of the Pods in your deployment.",
       attachTo: {
         element: "#resource-bars",
         on: "top",
@@ -350,7 +367,24 @@ export const getSizerTour: GetSizeTour = (dispatch) => {
       ],
     },
     {
-      text: "To download the cluster picture click here. You can then use this picture in your report or slide",
+      text: "At the bottom we show the total available CPU and Memory on this Machine.",
+      attachTo: {
+        element: ".pf-c-card__footer",
+        on: "top",
+      },
+      buttons: [
+        {
+          text: "Skip Tour",
+          action: tour.cancel,
+        },
+        {
+          text: "Next",
+          action: tour.next,
+        },
+      ],
+    },
+    {
+      text: "You can download a cluster picture here. Use this in your report or slides",
       attachTo: {
         element: "#screenshot-download",
         on: "top",
